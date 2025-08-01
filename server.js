7b8afa604d87709acc7eceb1eb94f34184ae76fb
Isaiah Chiraira <itchiraira@gmail.com>
2025-08-01 03:03:08 +0200

    added live frontend

--- server.js
+++ server.js
@@ -39,7 +39,9 @@ ensureDirectoriesExist();
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    // origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://opptapp.onrender.com",
+
     credentials: true,
   })
 );
